799608ce4a4f97d8ed6b96d266fa2d641b827fc4
DCLInter <juan.arbelaez-salazar@city.ac.uk>
2025-07-24 16:40:56 +0100

    Small changes and ML script

--- Scripts/bp_estimation.py
+++ Scripts/bp_estimation.py
@@ -2,33 +2,116 @@ import h5py
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
-from sklearn.datasets import load_breast_cancer
+import shap
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
 data_path = 'C:/Users/adhn565/Documents/Data/completo_conAttrs_16_7_25.h5'
-data_target = 'BP_values.h5'
+data_path_target = 'BP_values.h5'
 data = {}
+data_target = {}
 segment_ids = {}
 
-# with h5py.File(data_path, 'r') as f:
-#     for group_name in f:
-#         group = f[group_name]
-#         data[group_name] = {}
-#         for dtset_name in group:
-#             data[group_name][dtset_name] = group[dtset_name][()]
-#             data[group_name][dtset_name] = data[group_name][dtset_name]
-#         segment_ids[group_name] = group["segments"][0]
+def evaluate(true, pred, label):
+    mae = mean_absolute_error(true, pred)
+    rmse = np.sqrt(mean_squared_error(true, pred))
+    r2 = r2_score(true, pred)
     
-#     fiducial = f["p000001"]["segments"].attrs['fiducial_order']
-#     features = f["p000001"]["mean_p000001"].attrs['features']
-#     fiducial = [f.decode() if isinstance(f, bytes) else f for f in fiducial]
-#     features = [f.decode() if isinstance(f, bytes) else f for f in features]
+    print(f"{label} — MAE: {mae:.2f}, RMSE: {rmse:.2f}, R²: {r2:.3f}")
 
-datasource = load_breast_cancer()
-X_train, X_test, y_train, y_test = train_test_split(datasource.data, datasource.target, test_size=0.2)
-model = lgb.LGBMClassifier()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-print("Accuracy:", accuracy_score(y_test, y_pred))
+with h5py.File(data_path, 'r') as f:
+    for group_name in f:
+        group = f[group_name]
+        data[group_name] = {}
+        for dtset_name in group:
+            data[group_name][dtset_name] = group[dtset_name][()]
+            data[group_name][dtset_name] = data[group_name][dtset_name]
+        segment_ids[group_name] = group["segments"][0]
+    
+    fiducial = f[group_name]["segments"].attrs['fiducial_order']
+    features = f["p000001"]["mean_p000001"].attrs['features']
+    fiducial = [f.decode() if isinstance(f, bytes) else f for f in fiducial]
+    features = [f.decode() if isinstance(f, bytes) else f for f in features]
+
+with h5py.File(data_path_target, 'r') as f:
+    for group_name in f:
+        group = f[group_name]
+        data_target[group_name] = {}
+        for dtset_name in group:
+            data_target[group_name][dtset_name] = group[dtset_name][()]
+            data_target[group_name][dtset_name] = data_target[group_name][dtset_name]
+
+dataframe_X = pd.DataFrame(columns=features)
+for p in data.keys():
+    p_array = data[p][f"mean_{p}"].T
+    p_array = p_array[:len(p_array)//2]
+    p_df = pd.DataFrame(p_array,columns=features)
+    dataframe_X = pd.concat([dataframe_X,p_df],ignore_index=True)
+
+target_label = ["SBP","DBP","MAP"]
+df_target = pd.DataFrame(columns= target_label)
+for p in data.keys():
+    p_array = data_target[p]["Bp_values"].T
+    p_df = pd.DataFrame(p_array,columns= target_label)
+    df_target = pd.concat([df_target,p_df],ignore_index=True)
+
+imputer_X = SimpleImputer(strategy='median')
+X = imputer_X.fit_transform(dataframe_X)
+imputer_y = SimpleImputer(strategy='median')
+y = imputer_y.fit_transform(df_target)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = lgb.LGBMRegressor(random_state= 42, max_depth= 15, learning_rate= 0.05, n_estimators= 1000)
+multi_model = MultiOutputRegressor(model)
+multi_model.fit(X_train, y_train)
+
+predictions = multi_model.predict(X_test)
+sbp_pred = predictions[:, 0]
+dbp_pred = predictions[:, 1]
+map_pred = predictions[:, 2]
+
+evaluate(y_test[:, 0], sbp_pred, "SBP")
+evaluate(y_test[:, 1], dbp_pred, "DBP")
+evaluate(y_test[:, 2], map_pred, "MAP")
+
+for i in range(len(target_label)):
+    # Extract the i-th model from the MultiOutputRegressor
+    model_i = multi_model.estimators_[i]
+    # Use TreeExplainer (fast & efficient for LightGBM)
+    explainer = shap.Explainer(model_i)
+    shap_values = explainer(X[:10000])
+    # Summary plot
+    shap.summary_plot(shap_values, X[:10000], feature_names=dataframe_X.columns)
+
+for i in range(len(target_label)):
+    plt.figure()
+    plt.scatter(y_test[:, i], predictions[:, i], alpha=0.5)
+    plt.plot([y_test[:, i].min(), y_test[:, i].max()],
+             [y_test[:, i].min(), y_test[:, i].max()],
+             'r--')
+    plt.xlabel("True")
+    plt.ylabel("Predicted")
+    plt.title(f"{target_label[i]}: Predicted vs True")
+    plt.grid(True)
+    plt.show()
+
+# param_grid = {
+#     'estimator__num_leaves': [31, 50],
+#     'estimator__learning_rate': [0.01, 0.05],
+#     'estimator__n_estimators': [100, 200]
+# }
+# grid = GridSearchCV(
+#     MultiOutputRegressor(LGBMRegressor(random_state=42)),
+#     param_grid,
+#     cv=3,
+#     scoring='neg_mean_absolute_error',
+#     verbose=1,
+#     n_jobs=-1
+# )
+
+
+
--- Scripts/cleaning_comparison.py
+++ Scripts/cleaning_comparison.py
@@ -73,25 +73,23 @@ def outliers_IQRandMAD(array):
 
 
 def cleaningAnalysis(before: np.array,after: np.array):
-    
-    #### X IS THE FEATURE FROM THE NORMAL DATASET, Y IS THE FEATURE FROM THE CLEANED DATASET
+    #### "X" IS THE FEATURE FROM THE NORMAL DATASET, "Y" IS THE FEATURE FROM THE CLEANED DATASET
     x = before
     x = x[:len(x)//2]
     y = after
 
     ### change in standart statistics
     means = [np.mean(x),np.mean(y)]
-    means_change = 100*np.diff(means)[0]/means[0]
-
+    median = [np.median(x),np.median(y)]
     percentiles25 = [np.percentile(x,25), np.percentile(y,25)]
-    percentiles75 = [np.percentile(x,75), np.percentile(y,75)]
+    percentiles75 = [np.percentile(y,75), np.percentile(y,75)]
+    
+    means_change = 100*np.diff(means)[0]/means[0]
+    median_change = 100*np.diff(median)[0]/median[0]
     percentiles_change = [ 100*np.diff(percentiles25)[0]/percentiles25[0] , 100*np.diff(percentiles75)[0]/percentiles75[0] ]
 
     iqr = [scStats.iqr(x), scStats.iqr(y)]
     iqr_change = 100*np.diff(iqr)[0]/iqr[0]
-
-    median = [np.median(x),np.median(y)]
-    median_change = 100*np.diff(median)[0]/median[0]
 
     mad = [scStats.median_abs_deviation(x), scStats.median_abs_deviation(y)]
     mad_change = 100*np.diff(mad)[0]/mad[0]
@@ -117,34 +115,82 @@ def cleaningAnalysis(before: np.array,after: np.array):
 
 def byPatient(data: dict, data_clean: dict, features_names: list):
     change_perPatient = {}
+    outliers_perPatient = {}
+    number_overlapPatient = {}
     ft_names = features_names
     for patient in data.keys():
 
         ft = data[patient][f"mean_{patient}"]
         ft_clean = data_clean[patient][f"mean_{patient}"]
         change_perFeature = {}
+        outliers_perFeature = {}
+        number_overlapFeature = {}
 
         for feat in np.arange(len(ft_clean)):
             x_before = ft[feat]
             x_after = ft_clean[feat]
-            stat_changes = cleaningAnalysis(before = x_before, after = x_after)
-            change_perFeature[ft_names[feat]] = stat_changes
+            try:
+                stat_changes = cleaningAnalysis(before = x_before, after = x_after)
+                change_perFeature[ft_names[feat]] = stat_changes
 
+                outliers_val_bef, outliers_idx_bef = outliers_IQRandMAD(x_before)
+                outliers_val_aft, outliers_idx_aft = outliers_IQRandMAD(x_after)
+
+                overlap_iqr = np.where(outliers_idx_bef["IQR"] == outliers_idx_aft["IQR"])[0]
+                overlap_mad = np.where(outliers_idx_bef["MAD"] == outliers_idx_aft["MAD"])[0]
+            except:
+                print(f"{patient}, {features[feat]}\nOne of the arrays its empty: \n normal data (size): {x_before.size}\n clean data (size): {x_after.size}")
+
+            outliers_perFeature[ft_names[feat]] = {
+            "values_original": outliers_val_bef,
+            "values_cleaned": outliers_val_aft,
+            "indexes_original": outliers_idx_bef,
+            "indexes_cleaned": outliers_idx_aft
+            }
+
+            overlap_score_iqr = 0
+            if len(overlap_iqr) > 0:
+                v1 = outliers_val_bef["IQR"]
+                v2 = outliers_val_aft["IQR"]
+                total = len(v1) + len(v2)
+                if v1.size == 0 or v2.size == 0:
+                    pass
+                else:
+                    n = len(overlap_iqr)
+                    overlap_score_iqr = n/total
+            overlap_score_mad = 0
+            if len(overlap_mad) > 0:
+                v1 = outliers_val_bef["MAD"]
+                v2 = outliers_val_aft["MAD"]
+                if v1.size == 0 or v2.size == 0:
+                    pass
+                else:
+                    m = len(overlap_mad)
+                    overlap_score_mad = m/(len(v1) + len(v2))
+
+            number_overlapFeature[ft_names[feat]] = {
+                "IQR": overlap_score_iqr,
+                "MAD": overlap_score_mad
+            }
+
+        number_overlapPatient[patient] = number_overlapFeature
+        outliers_perPatient[patient] = outliers_perFeature
         change_perPatient[patient] = change_perFeature
     
-    return change_perPatient
+    return change_perPatient, number_overlapPatient, outliers_perPatient
 
 def byFeature_medians(data: dict, data_clean: dict, features_names: list):
     median_all = {}
     number_overlap = {}
     ft_names = features_names
-    for f in ft_names:
+    for ft in ft_names:
         feat_values = []
         feat_values_clean = []
         for p in data.keys():
-            idx_ft = search_feat(f,ft_names)
+            idx_ft = search_feat(ft,ft_names)
             x = data[p][f"mean_{p}"][idx_ft]
-            x = x[:len(x)//2]
+            if x.size > 10:
+                x = x[:len(x)//2]
             y = data_clean[p][f"mean_{p}"][idx_ft]
 
             median_x = np.median(x)
@@ -155,8 +201,9 @@ def byFeature_medians(data: dict, data_clean: dict, features_names: list):
 
         array_before = np.array(feat_values)
         array_after = np.array(feat_values_clean)
+
         stat_changes = cleaningAnalysis(before = array_before, after = array_after)
-        median_all[f] = stat_changes
+        median_all[ft] = stat_changes
 
         ### Outliers
         otl_val_x, otl_idx_x = outliers_IQRandMAD(array_before)
@@ -164,6 +211,13 @@ def byFeature_medians(data: dict, data_clean: dict, features_names: list):
 
         overlap_iqr = np.where(otl_idx_x["IQR"] == otl_idx_y["IQR"])[0]
         overlap_mad = np.where(otl_idx_x["MAD"] == otl_idx_y["MAD"])[0]
+
+        outliers = {
+            "values_original": otl_val_x,
+            "values_cleaned": otl_val_y,
+            "indexes_original": otl_idx_x,
+            "indexes_cleaned": otl_idx_y
+        }
 
         overlap_score_iqr = 0
         if len(overlap_iqr) > 0:
@@ -185,23 +239,24 @@ def byFeature_medians(data: dict, data_clean: dict, features_names: list):
                 m = len(overlap_mad)
                 overlap_score_mad = m/(len(v1) + len(v2))
 
-        number_overlap[f] = {
+        number_overlap[ft] = {
             "IQR": overlap_score_iqr,
             "MAD": overlap_score_mad
         }
     
-    return median_all, number_overlap
+    return median_all, number_overlap, outliers
 
 def byFeature_all(data: dict, data_clean: dict, features_names: list):
     signals_all = {}
     ft_names = features_names
-    for f in ft_names:
+    for ft in ft_names:
         feat_values = []
         feat_values_clean = []
         for p in data.keys():
-            idx_ft = search_feat(f,ft_names)
+            idx_ft = search_feat(ft,ft_names)
             x = data[p][f"mean_{p}"][idx_ft]
-            x = x[:len(x)//2]
+            if x.size > 10:
+                x = x[:len(x)//2]
             y = data_clean[p][f"mean_{p}"][idx_ft]
 
             feat_values.extend(x)
@@ -210,11 +265,27 @@ def byFeature_all(data: dict, data_clean: dict, features_names: list):
         array_before = np.array(feat_values)
         array_after = np.array(feat_values_clean)
         stat_changes = cleaningAnalysis(before = array_before, after = array_after)
-        signals_all[f] = stat_changes
+        signals_all[ft] = stat_changes
 
     return signals_all
 
-b = byFeature_medians(data,data_clean,features)
-# a = byFeature_all(data,data_clean,features)
+st_ch_p, num_p, outl_p = byPatient(data,data_clean,features)
+st_ch_med, num_med, outl_med = byFeature_medians(data,data_clean,features)
+st_ch_all = byFeature_all(data,data_clean,features)
 
+print(num_p)
+# iqrs = {}
+# for key, value in num_p.items():
+#     liqrs = []
+#     for k, v in value.items():
+#         for ft, dt in v.items():
+#             liqrs.append(dt[])
+#     iqrs[key] = liqrs
+# print(iqrs)
+
+# print(num_med)
+
+# a = pd.DataFrame(data_clean["p000610"]["mean_p000610"])
+# b = pd.DataFrame(data["p000366"]["mean_p000366"])
+# print(b)
 
